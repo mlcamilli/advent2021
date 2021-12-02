236c94744f3ca79ed6d48567a36c77e2341c7070
Matt Camilli <matt@camilli.dev>
2021-12-01 21:15:30 -0500

    day1 part2

--- day1.py
+++ day1.py
@@ -11,5 +11,26 @@ def get_increases():
         print(f"{increases} total increases.")
 
 
+def get_thruple_increases():
+    with open("day1.txt", "r") as f:
+        text = f.read()
+        raw_values = text.split("\n")
+        values = [val for val in raw_values if val]
+        increases = 0
+        thruples = []
+        for i in range(0, len(values)):
+            if i > len(values) - 3:
+                break
+            total_val = sum([int(val) for val in values[i : i + 3]])
+            thruples.append(total_val)
+
+        for i, val in enumerate(thruples):
+            if i == 0 or not (val):
+                continue
+            if int(val) > int(thruples[i - 1]):
+                increases += 1
+        print(f"{increases} total increases.")
+
+
 if __name__ == "__main__":
-    get_increases()
+    get_thruple_increases()
